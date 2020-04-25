5aa48366d75cf15411cf342abbc7d3c491d4267f
Sri Lakshmi <Sri Lakshmi@DESKTOP-OVN321D>
2020-04-25 13:06:41 +0530

    1st change

--- DemoProj2/src/com/nt/beans/Utility.java
+++ DemoProj2/src/com/nt/beans/Utility.java
@@ -3,7 +3,8 @@ package com.nt.beans;
 import java.util.Date;
 
 public class Utility {
-	public Date getSysDate() {
+	public Date getSysDate() { 
+		System.out.println("Utility.getSysDate()");
 		return new Date();
 	}
 
