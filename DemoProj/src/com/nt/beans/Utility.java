718e89616c73817b2b6eaacd8f01271e4ef533a7
Sri Lakshmi <Sri Lakshmi@DESKTOP-OVN321D>
2020-04-28 12:37:31 +0530

    fdgfhgj

--- DemoProj/src/com/nt/beans/Utility.java
+++ DemoProj/src/com/nt/beans/Utility.java
@@ -14,8 +14,13 @@ public class Utility {
 	public void m2() {
 		System.out.println("Utility.m2()");
 	}
+
 	public void m4() {
 		System.out.println("Utility.m4()");
+    }
+	public void m3() {
+		System.out.println("Utility.m3()");
+		
 	}
 
 }
